8a95173e81347e4a791ecc43c2be361bbea1ea04
gusalviarez <gusalv2001@gmail.com>
2024-06-28 20:26:33 -0400

    add anagram_groups

---
+++ js/array_and_hashing/anagram_groups.js
@@ -0,0 +1,27 @@
+/*
+Given an array of strings strs, group all anagrams together into sublists. You may return the output in any order.
+
+An anagram is a string that contains the exact same characters as another string, but the order of the characters can be different.
+
+Example 1:
+Input: strs = ["act","pots","tops","cat","stop","hat"]
+Output: [["hat"],["act", "cat"],["stop", "pots", "tops"]]
+*/
+
+let strs = ["act","pots","tops","cat","stop","hat"]
+
+const group_anagram = (strs) => {
+    let map = new Map()
+    for (let str of strs) {
+        let sorted = str.split('').sort().join('')
+        if (map.has(sorted)) {
+            map.get(sorted).push(str)
+        } else {
+            map.set(sorted, [str])
+        }
+    }
+    console.log(Array.from(map.values()))
+    return Array.from(map.values())
+}
+
+group_anagram(strs)
