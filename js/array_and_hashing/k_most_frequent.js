0e8a1709bc857d2812d47853e6eb65456f1cb6c0
gusalviarez <gusalv2001@gmail.com>
2024-07-04 19:15:34 -0400

    add k most frequent

---
+++ js/array_and_hashing/k_most_frequent.js
@@ -0,0 +1,36 @@
+const kMostFrequent = function(nums, k) {
+    // Frequency counter
+    let freqs = {};
+
+
+    // iterate nums, if is a new num them create 1 if not then add 1
+    for (num of nums) {
+        if (freqs[num] === undefined) { 
+            freqs[num] = 1;
+        } else {
+            freqs[num] = freqs[num] + 1;
+        }
+    }
+    
+    // Convert to array with [frequency, number] elements
+    let frequencyArray = [];
+    for (key in freqs) {
+        frequencyArray.push([freqs[key], key]);
+    }
+    
+    // Sort in descending order with frequency as key
+    frequencyArray.sort((a, b) => {
+        return b[0] - a[0];
+    });
+    
+    // Get most frequent element out of array
+    mostFreq = [];
+    for (let i = 0; i < k; i++) {
+        mostFreq.push(frequencyArray[i][1]);
+    }
+    return mostFreq;
+};
+
+k = 2
+nums = [1,1,2,2,2,3,4,4,4]
+console.log(kMostFrequent(nums, k))
