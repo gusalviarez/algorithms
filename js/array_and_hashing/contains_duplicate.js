f913807283562d68ebb29264263b0147bde1e51b
gusalviarez <gusalv2001@gmail.com>
2024-07-08 09:13:44 -0400

    add second version contains_duplicate

--- js/array_and_hashing/contains_duplicate.js
+++ js/array_and_hashing/contains_duplicate.js
@@ -7,13 +7,32 @@ Input: nums = [1,2,3,1]
 Output: true
  */
 
-nums = [1,2,3,1]
+nums = [1, 2, 3, 1];
 
 const containsDuplicate = (nums) => {
-    // use a Set to get the nums values without duplicate values
-    comparisonSet = [new Set(nums)] 
-    // if the lengths are different, it means there are duplicate
-    return comparisonSet.length !== nums.length
-} 
+  // use a Set to get the nums values without duplicate values
+  comparisonSet = [new Set(nums)];
+  // if the lengths are different, it means there are duplicate
+  return comparisonSet.length !== nums.length;
+};
 
-console.log(containsDuplicate(nums))
+function containsDuplicate2(nums) {
+  // Create an empty set to store seen numbers
+  const seen = new Set();
+
+  // Loop through the array
+  for (const num of nums) {
+    // Check if the number is already in the set
+    if (seen.has(num)) {
+      return true; // duplicate detected, return true
+    }
+    // Add the number to the set
+    seen.add(num);
+  }
+
+  // No duplicate found, return false
+  return false;
+}
+
+console.log(containsDuplicate(nums));
+console.log(containsDuplicate2(nums));
